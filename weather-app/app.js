d845c537fc7eef296c1da6125a573a314ec7c124
TanishqSingla <tanishqsingla00@gmail.com>
2020-03-24 22:50:49 +0530

    Added weather api

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,7 @@
 const request = require("request");
 
 const url =
-  "https://api.darksky.net/forecast/a75189e53de8cdc42a2eb8e7bdc1c4d7/37.8267,-122.4233";
+  "https://api.darksky.net/forecast/a75189e53de8cdc42a2eb8e7bdc1c4d7/37.8267,-122.4233?unit=si";
 
 request({ url: url, json: true }, (error, response) => {
   const currentRes = response.body.currently;
@@ -10,4 +10,20 @@ request({ url: url, json: true }, (error, response) => {
       currentRes.temperature
     }. There is ${currentRes.precipProbability * 100}% probablity`
   );
+});
+
+//Geocoding
+const token =
+  "https://eu1.locationiq.com/v1/search.php?key=d92749dd0546c9&q=Los%20Angeles&format=json";
+
+request({ url: url, json: true }, (error, response) => {
+  if (error) {
+    console.log("Unable to connect to the service");
+  } else if (response.body.error) {
+    console.log("Cannot find location");
+  } else {
+    const lat = response.body[0].lat;
+    const lon = response.body[0].lon;
+    console.log(lat, lon);
+  }
 });
